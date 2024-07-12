0a69baa01d3f0a9ebf5f88109e49d69eebf3ba5f
sanas12 <162470494+sanas12@users.noreply.github.com>
2024-07-12 19:24:55 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+// import the screens
+import Screen1 from "./components/Screen1";
+import Screen2 from "./components/Screen2";
+
+// import react Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen1">
+        <Stack.Screen name="Screen1" component={Screen1} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
